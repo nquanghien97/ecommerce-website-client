79ed4ada0f9dff3379b89f6f8dd81e2fb78f7333
nquanghien <nquanghien97@gmail.com>
2023-03-18 18:17:47 +0700

    show message when filter page dont have products

--- src/pages/FilterProducts.jsx
+++ src/pages/FilterProducts.jsx
@@ -57,6 +57,14 @@ function Product() {
     )
   }
 
+  if(product?.length === 0) {
+    return (
+      <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <h1>Không có sản phẩm phù hợp</h1>
+      </div>
+    )
+  }
+
   const filterName = pathname.slice(1, pathname.length)
 
   return (
