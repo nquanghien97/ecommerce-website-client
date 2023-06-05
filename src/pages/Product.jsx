2bb21d7e5335933226052235450b4f600649be86
nquanghien <nquanghien97@gmail.com>
2023-06-05 21:12:17 +0700

    fix Product page UI

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import styled from 'styled-components'
 import { useParams } from 'react-router-dom';
+import { Container, Button, Typography, Box, CircularProgress } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { AddCart } from '../redux/Products/actions';
-import { mobile } from '../responsive';
 import { getProduct } from '../api/productServices';
-import { Box, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import WishListIcon from '../components/common/WishListIcon';
 import { openSnackBar } from '../redux/User/user.actions';
@@ -18,6 +16,62 @@ const useStyles = makeStyles({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    display: 'flex',
+    marginTop: '110px',
+    '@media(max-width: 1050px)' : {
+      marginTop: '152px',
+      flexDirection: 'column',
+    }
+  },
+  img: {
+    width: '100%',
+  },
+  left: {
+    width: 'calc(100% - 456px)',
+    padding: '0 48px',
+    '@media(max-width: 1050px)' : {
+      width: '100%'
+    }
+  },
+  name: {
+    fontSize: '24px'
+  },
+  description: {
+    fontWeight: '600',
+    fontSize: '20px',
+  },
+  price: {
+    padding: '12px 0',
+    fontSize: '20px',
+    fontWeight: '600',
+  },
+  right: {
+    width: '456px',
+    '@media(max-width: 1050px)' : {
+      width: '100%',
+      padding: '12px 48px',
+    }
+  },
+  btnGroup: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  likeIcon: {
+    cursor: 'pointer',
+    padding: '10px',
+    border: '1px solid',
+    borderRadius: '5px',
+    marginLeft: '12px'
+  },
+  detail: {
+    paddingTop: '24px',
+  },
+  textDetail: {
+    lineHeight: '1.75',
+    fontSize: '18px',
+  }
 })
 
 function Product() {
@@ -68,113 +122,40 @@ function Product() {
   const { _id, price} = product
 
   return (
-    <Container>
-      <Left>
-        <Image src={product.imageUrl} />
-      </Left>
-      <Right>
-        <Description>
-          <Title>{product.name}</Title>
-          <Price>{Number(product.price).toLocaleString('en-US')}đ</Price>
-        </Description>
-        <BuySection>
-          <Size>
+    <Container className={classes.container}>
+      <Box className={classes.left}>
+        <img className={classes.img} src={product.imageUrl} alt="" />
+      </Box>
+      <Box className={classes.right}>
+        <Box>
+          <Typography className={classes.name}>{product.name}</Typography>
+          <Typography className={classes.description}>{product.description}</Typography>
+          <Typography className={classes.price}>{Number(product.price).toLocaleString('en-US')}đ</Typography>
+        </Box>
+        <Box>
+          <Box>
 
-          </Size>
-          <AddWrapper>
-            <AddCard onClick={()=> addCart(_id, price, userId)}>
+          </Box>
+          <Box className={classes.btnGroup}>
+            <Button
+              onClick={()=> addCart(_id, price, userId)}
+              variant="contained"
+              fullWidth
+              style={{backgroundColor: '#3f51b5', color: 'white', padding: '12px 24px'}} 
+            >
               Thêm vào giỏ hàng
-            </AddCard>
-            <AddWishListWrapper>
+            </Button>
+            <Box className={classes.likeIcon}>
               <WishListIcon item={product} liked={WishList ? WishList.filter(like => like._id === product._id).length > 0 ? true : false : false} />
-            </AddWishListWrapper>
-          </AddWrapper>
-        </BuySection>
-      </Right>
+            </Box>
+          </Box>
+          <Box className={classes.detail}>
+            <Typography className={classes.textDetail}>{product.detail}</Typography>
+          </Box>
+        </Box>
+      </Box>
     </Container>
   )
 }
-
- 
-const Container = styled.div`
-  display: flex;
-  margin-top: 110px;
-  ${mobile({
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    marginTop: '152px',
-  })}
-`
-const Left = styled.div`
-  display: flex;
-  justify-content: center;
-  flex: 60%;
-  padding: 20px;
-  ${mobile({
-    maxWidth: '550px',
-  })}
-`
-const Image = styled.img`
-  width: 60%;
-  border: 1px solid;
-  border-radius: 4px;
-  ${mobile({
-    width: '100%',
-  })}
-`
-const Right = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 30%;
-  padding: 20px;
-  margin-right: 80px;
-  ${mobile({
-    marginRight: '0 !important',
-  })}
-`
-const Description = styled.div`
-  width: 100%;
-`
-const Title = styled.h1`
-  margin-bottom: 12px;
-`
-const Price = styled.p`
-  font-size: 24px;
-  margin-bottom: 12px;
-`
-const BuySection = styled.div`
-
-`
-const Size = styled.div`
-
-`
-const AddWrapper = styled.div`
-  display: flex;
-`
-const AddCard = styled.button`
-  width: 90%;
-  padding: 12px;
-  border-radius: 4px;
-  border: 1px solid;
-  cursor: pointer;
-  &:hover {
-    background-color: black;
-    color: white;
-  }
-`
-const AddWishListWrapper = styled.div`
-  margin: 0 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px;
-  border: 1px solid;
-  border-radius: 4px;
-  cursor: pointer;
-  &:hover{
-    background-color: pink;
-  }
-` 
 
 export default Product;
